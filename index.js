cf5edefd9de9724e3d22097bce0ea3c7312cf0ab
Scott Green <sagreenxyz@gmail.com>
2022-02-21 20:08:22 -0600

    waypoint

--- index.js
+++ index.js
@@ -5,10 +5,37 @@ const app = express();
 
 app.get('/', function (req, res) {
     res.send(`
-        <body>
-            <h1>Hello World</h1>
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: white;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1>White Page</h1>
         </body>
     `);
+})
+
+// Teal Page
+app.get('/teal', function (req, res) {
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: teal;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1 style="color: teal;">Teal Page</h1>
+        </body>
+    `)
+})
+
+// Crimson Page
+app.get('/crimson', function (req, res) {
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: crimson;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1 style="color: crimson;">Crimson Page</h1>
+        </body>
+    `)
 })
 
 app.listen(process.env.PORT, () => {
