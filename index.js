9330ac76383e69319e2f82f48cb209e89f002b08
Scott Green <sagreenxyz@gmail.com>
2022-02-21 19:59:55 -0600

    Basic Express Template

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('I am here!...');
+});
+
