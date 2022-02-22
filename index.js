dc45a1efce28c7491ba4f3cbcc283a270d24bebb
Scott Green <sagreenxyz@gmail.com>
2022-02-21 20:03:23 -0600

    waypoint

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send(`
+        <body>
+            <h1>Hello World</h1>
+        </body>
+    `);
 })
 
 app.listen(process.env.PORT, () => {
